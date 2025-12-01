8563892a3aa30d2496f1ae998c4ba0b442fdc250
Mike Lythgoe <github@mikelythgoe.net>
2025-12-01 21:01:49 +0000

    renaming of variables

--- SimpleTcpEchoClient.cs
+++ SimpleTcpEchoClient.cs
@@ -1,16 +1,15 @@
 ﻿using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using HL7.Dotnetcore;
 
-TcpClient ourTcpClient = null;
-NetworkStream networkStream = null;
+TcpClient? ourTcpClient = null;
+NetworkStream? networkStream = null;
 
 try
 
 {
-    //initiate a TCP client connection to local loopback address at port 1080
-    ourTcpClient = new System.Net.Sockets.TcpClient();
+    //initiate a TCP client connection to a local loopback address at port 1080
+    ourTcpClient = new TcpClient();
 
     ourTcpClient.Connect(new IPEndPoint(IPAddress.Loopback, 1080));
 
@@ -20,7 +19,7 @@ try
     networkStream = ourTcpClient.GetStream();
 
     //use UTF-8 and either 8-bit encoding due to MLLP-related recommendations
-    var messageToTransmit =
+    var sentMessage =
         "MSH|^~\\&|EPIC|SIISCLIENT818^LINDAS TEST ORGANIZATION|^SIIS||20150202115044||VXU^V04^VXU_V04|225|P|2.5.1||||AL|\n" +
         "PID|1||E46700^^^^MR^||DOE^JOHN^C^JR^^^L|SMITH|20140515|M|SMITH^JOHN|2106-3^WHITE^HL70005|115 MAINSTREET^^GOODTOWN^KY^42010^USA^L^010||^PRN^PH^^^270^6009800||EN^ENGLISH^HL70296||||523968712|||2186-5^NOT HISPANIC OR LATINO^HL70012||||||||N|\n" +
         "PD1|||||||||||02^Reminder/recall-any method^HL70215|||||A^Active^HL70441|20150202^20150202 NK1|1|DOE^MARY|MTH^MOTHER^HL70063|\n" +
@@ -29,14 +28,14 @@ try
         "OBX|2|CE|30956-7^VACCINE TYPE^LN|2|88^FLU^CVX||||||F|||20150202102525 OBX|3|TS|29768-9^Date vaccine information statement published^LN|2|20120702||||||F OBX|4|TS|29769-7^Date vaccine information statement presented^LN|2|20120202||||||F\n" +
         "RXA|0|1|20141215|20141115|141^influenza, SEASONAL 36^CVX^90658^Influenza Split^CPT|999|||01^HISTORICAL INFORMATION – SOURCE UNSPECIFIED^ NIP001||||||||||||A";
 
-    var byteBuffer = Encoding.UTF8.GetBytes(messageToTransmit);
+    var byteBuffer = Encoding.UTF8.GetBytes(sentMessage);
 
     //send a message through this connection using the IO stream
     networkStream.Write(byteBuffer, 0, byteBuffer.Length);
 
     Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Sending to Server");
 
-    Console.WriteLine(messageToTransmit);
+    Console.WriteLine(sentMessage);
     
     Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Sent to Server");
 
